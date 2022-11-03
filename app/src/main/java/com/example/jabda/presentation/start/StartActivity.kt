d50073a5548a55cc87777d6fc8a03261703bdd01
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-11-03 15:04:43 +0900

    사원 회원가입 페이지

--- app/src/main/java/com/example/jabda/presentation/start/StartActivity.kt
+++ app/src/main/java/com/example/jabda/presentation/start/StartActivity.kt
@@ -1,8 +1,10 @@
 package com.example.jabda.presentation.start
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.jabda.databinding.ActivityStartBinding
+import com.example.jabda.presentation.login.LoginActivity
 
 class StartActivity: AppCompatActivity() {
     lateinit var binding: ActivityStartBinding
@@ -10,5 +12,8 @@ class StartActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityStartBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.loginBtn.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
+        }
     }
 }

7559ef3fda0458b008ff9d7f11a12008ea188657
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-11-04 00:05:57 +0900

    사원증 지문인식 후 조회

--- app/src/main/java/com/example/jabda/presentation/card/CardFragment.kt
+++ app/src/main/java/com/example/jabda/presentation/card/CardFragment.kt
@@ -1,11 +1,6 @@
 package com.example.jabda.presentation.card
 
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
-import android.nfc.NfcAdapter
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,6 +9,7 @@ import androidx.fragment.app.activityViewModels
 import com.bumptech.glide.Glide
 import com.example.jabda.databinding.FragmentCardBinding
 import com.example.jabda.presentation.main.MainViewModel
+
 
 class CardFragment: Fragment() {
     private lateinit var binding: FragmentCardBinding
@@ -25,7 +21,13 @@ class CardFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentCardBinding.inflate(inflater, container, false)
-//        viewModel.setIsCard(true)
+        viewModel.setIsApprove(true)
+        viewModel.isMe.observe(viewLifecycleOwner) {
+            if (it) {
+                viewModel.setIsCard(true)
+                binding.cardLayout.visibility = View.VISIBLE
+            }
+        }
         Glide.with(this)
             .load("https://static-whale.pstatic.net/main/img_darkmode_v12@2x.png")
             .circleCrop()
--- app/src/main/java/com/example/jabda/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/example/jabda/presentation/main/MainViewModel.kt
@@ -4,10 +4,22 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
+    private val _isApprove = MutableLiveData<Boolean>()
     private val _isCard = MutableLiveData<Boolean>()
+    private val _isMe = MutableLiveData<Boolean>()
     val isCard get() = _isCard
+    val isApprove get() = _isApprove
+    val isMe get() = _isMe
+
 
     fun setIsCard(isCard: Boolean) {
         _isCard.value = isCard
+    }
+    fun setIsApprove(isApprove: Boolean) {
+        _isApprove.value = isApprove
+    }
+
+    fun setIsMe(isMe: Boolean) {
+        _isMe.value = isMe
     }
 }

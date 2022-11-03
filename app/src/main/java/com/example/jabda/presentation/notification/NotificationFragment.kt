e0666403a18dcc46e165ac2cbaa19a7b926c91a0
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-11-04 04:56:36 +0900

    관리자에 따라 공지글 작성 버튼 생성

--- app/src/main/java/com/example/jabda/presentation/notification/NotificationFragment.kt
+++ app/src/main/java/com/example/jabda/presentation/notification/NotificationFragment.kt
@@ -5,21 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.jabda.adapter.NotificationListAdapter
 import com.example.jabda.databinding.FragmentNotificationBinding
 import com.example.jabda.network.response.notices.NoticesResponse
+import com.example.jabda.presentation.main.MainOwnerViewModel
 import java.util.*
 
 class NotificationFragment: Fragment() {
     lateinit var binding: FragmentNotificationBinding
     lateinit var adapter: NotificationListAdapter
+    val viewModel by activityViewModels<MainOwnerViewModel>()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentNotificationBinding.inflate(inflater, container, false)
+        if (viewModel.isOwner.value == true) {
+            binding.plusBtn.visibility = View.VISIBLE
+        }
         val item = NoticesResponse.Notice("", Date())
         adapter = NotificationListAdapter(NoticesResponse(listOf(item, item, item, item, item)))
         binding.notificationList.adapter = adapter

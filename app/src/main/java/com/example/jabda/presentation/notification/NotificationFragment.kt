6cb1fd6f9243d5c66e37afcde40aafced046d691
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-11-04 04:09:57 +0900

    item_notification 퍼블리싱

--- app/src/main/java/com/example/jabda/presentation/notification/NotificationFragment.kt
+++ app/src/main/java/com/example/jabda/presentation/notification/NotificationFragment.kt
@@ -5,16 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.jabda.adapter.NotificationListAdapter
 import com.example.jabda.databinding.FragmentNotificationBinding
+import com.example.jabda.network.response.notices.NoticesResponse
+import java.util.*
 
 class NotificationFragment: Fragment() {
     lateinit var binding: FragmentNotificationBinding
+    lateinit var adapter: NotificationListAdapter
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentNotificationBinding.inflate(inflater, container, false)
+        val item = NoticesResponse.Notice("", Date())
+        adapter = NotificationListAdapter(NoticesResponse(listOf(item, item, item, item, item)))
+        binding.notificationList.adapter = adapter
+        binding.notificationList.layoutManager = LinearLayoutManager(context)
         return binding.root
     }
 }

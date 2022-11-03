d15c305b4f258cdda68b03ec3bd81d19ae40198e
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-11-04 03:39:55 +0900

    관리자 페이지 스택관리

--- app/src/main/java/com/example/jabda/presentation/main/MainOwnerViewModel.kt
+++ app/src/main/java/com/example/jabda/presentation/main/MainOwnerViewModel.kt
@@ -5,9 +5,14 @@ import androidx.lifecycle.ViewModel
 
 class MainOwnerViewModel: ViewModel() {
     private val _isNotification = MutableLiveData<Boolean>()
+    private val _isPending = MutableLiveData<Boolean>()
     val isNotification get() = _isNotification
+    val isPending get() = _isPending
 
     fun setIsNotification(isNotification: Boolean) {
         _isNotification.value = isNotification
+    }
+    fun setIsPending(isPending: Boolean) {
+        _isPending.value = isPending
     }
 }

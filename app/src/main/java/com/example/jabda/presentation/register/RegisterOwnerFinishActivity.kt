d02879dda4d16e3128820030cf64a920528211b7
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2022-11-04 03:55:14 +0900

    회원가입 입력되는값 제대로 들어오는지 확인

--- app/src/main/java/com/example/jabda/presentation/register/RegisterOwnerFinishActivity.kt
+++ app/src/main/java/com/example/jabda/presentation/register/RegisterOwnerFinishActivity.kt
@@ -2,6 +2,8 @@ package com.example.jabda.presentation.register
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.jabda.databinding.ActivityRegisterOwnerFinishBinding
 import com.example.jabda.presentation.main.MainOwnerActivity
@@ -13,8 +15,24 @@ class RegisterOwnerFinishActivity: AppCompatActivity() {
         binding = ActivityRegisterOwnerFinishBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.finishBtn.setOnClickListener {
-            startActivity(Intent(this, MainOwnerActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
-            finish()
+            if(binding.inManagerCp.text.toString().isNullOrEmpty()){
+                Toast.makeText(this, "회사명을 입력해주세요.", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                if(binding.inManagerEpnum.text.toString().isNullOrEmpty()){
+                    Toast.makeText(this, "회사 사업자 번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    if((binding.radioBt1.isChecked || binding.rdioBt2.isChecked) && (binding.radioBt3.isChecked || binding.radioBt4.isChecked)){
+                        startActivity(Intent(this, MainOwnerActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
+                        finish()
+                        Log.d("상태","라 ${(binding.radioBt1.isChecked || binding.rdioBt2.isChecked) && (binding.radioBt3.isChecked || binding.radioBt4.isChecked)}")
+                    }
+                    else{
+                        Toast.makeText(this, "선택적 근로시간제 나 시차출퇴근제 또는 재택근무 허용 여부를 알려주세요", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
         }
     }
 }

0d9c1ccc6abfe22664db8eb592db70189e975165
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2022-11-04 01:02:18 +0900

    Home 프레그먼트 작업중

--- app/src/main/java/com/example/jabda/presentation/homework/HomeWorkFragment.kt
+++ app/src/main/java/com/example/jabda/presentation/homework/HomeWorkFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.jabda.databinding.FragmentHomeworkBinding
 import com.example.jabda.presentation.main.MainViewModel
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class HomeWorkFragment: Fragment() {
     lateinit var binding: FragmentHomeworkBinding
@@ -19,6 +24,11 @@ class HomeWorkFragment: Fragment() {
     ): View? {
         viewModel.setIsCard(false)
         binding = FragmentHomeworkBinding.inflate(inflater, container, false)
+        binding.calendar.setOnDateChangeListener{ view, year,month,dayOfMonth ->
+            var day : String = "${year}년 ${month}월 ${dayOfMonth}일"
+
+            binding.date.text = day
+        }
         return binding.root
     }
 }

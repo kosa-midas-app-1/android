b26459e191625c5288dfb59b8ad5b52d2dbc36cd
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-11-04 03:27:05 +0900

    관리자 메인에서 요청자 페이지로 이동

--- app/src/main/java/com/example/jabda/presentation/home/HomeOwnerFragment.kt
+++ app/src/main/java/com/example/jabda/presentation/home/HomeOwnerFragment.kt
@@ -5,16 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.findNavController
+import com.example.jabda.R
 import com.example.jabda.databinding.FragmentHomeOwnerBinding
+import com.example.jabda.presentation.main.MainOwnerViewModel
 
 class HomeOwnerFragment: Fragment() {
     private lateinit var binding: FragmentHomeOwnerBinding
+    val viewModel by activityViewModels<MainOwnerViewModel>()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeOwnerBinding.inflate(inflater, container, false)
+        binding.registerCntLayout.setOnClickListener {
+            activity?.findNavController(R.id.fragment_club)?.navigate(R.id.action_homeFragment_to_pendingRegisterFragment)
+            viewModel.setIsNotification(true)
+        }
+        binding.homeWorkCntLayout.setOnClickListener {
+            activity?.findNavController(R.id.fragment_club)?.navigate(R.id.action_homeFragment_to_pendingHomeWorkFragment)
+            viewModel.setIsNotification(true)
+        }
         return binding.root
     }
 }
